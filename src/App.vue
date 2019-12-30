e2ff0554176b0f0afe0db7c80606afed7ae1eda0
Debbie <debiurquhart@live.co.uk>
2019-12-30 14:47:44 +0000

    fetches data from api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <h1>American Breweries</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      breweries: []
+    };
+  },
+
+  mounted() {
+    fetch("https://api.openbrewerydb.org/breweries")
+      .then(result => result.json())
+      .then(brewery => (this.breweries = brewery));
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="css" scoped></style>
